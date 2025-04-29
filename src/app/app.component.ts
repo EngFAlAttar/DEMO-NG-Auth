2461dcc9c3e4f6ca12b6267181b4e37b14f20238
EngFAlAttar <engfalattar@gmail.com>
2025-04-29 12:30:56 +0300

    WIP: UI/UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
